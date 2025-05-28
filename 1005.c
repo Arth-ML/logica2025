caacd017007bc6f42edd9ae3a6fe6e9cce3b9691
Arthur Rodrigues <arthrodrigues25@gmail.com>
2025-05-28 20:39:14 -0300

    .

---
+++ 1005.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+
+    double a, b, MEDIA;
+
+    scanf("%lf", &a);
+    scnaf("%lf", &b);
+
+    MEDIA = (3.5 * a) * (7.5 *  b) / 2;
+
+    printf("MEDIA = %.5f\n", MEDIA);
+
+
+
+
+
+
+
+    return 0;
+}
